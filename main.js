4eb7146ff657f17628b83f9e2f97748301ce7a42
Axe1lyze <axe1lyze@gmail.com>
2024-12-28 00:55:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,19 +14,18 @@
     var script = document.createElement('script');
     script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
     script.onload = ()=>{
-        
+        var userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36';
         function crawl(url, total) {
             $.ajax({
                 url: url,
-                headers:{
-                    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-                },
+                headers: { 'User-Agent': userAgent },
                 success: (resp)=>{
                     var ifr = document.createElement("iframe");
                     ifr.name = 'ifr_'+new Date().getTime();
                     ifr.style = 'display: none;';
                     ifr.callback = ()=>{
                         var doc = ifr.contentDocument;
+                        Object.defineProperty(ifr.window.navigator, 'userAgent', userAgent);
                         var pageTotal = Array.from(doc.querySelectorAll('.order-card div.a-column:nth-child(2) div:nth-child(2)'))
                             .map(i=>Number(i.innerText.replace(/[^0-9.]/gi,'')))
                             .reduce((a,b)=>a+b,0)
